7046a312b5b87bf5c1dc9e35ec14528f0fc30801
OstrichRun <347647092@qq.com>
2015-03-04 17:33:48 +0800

    iOS
    拆分

---
+++ src/iOS/DoExt_Notification_SM/ext_implement/DoExt_Notification_SM.h
@@ -0,0 +1,24 @@
+//
+//  TYPEID_SM.h
+//  DoExt_API
+//
+//  Created by @userName on @time.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import "DoExt_Notification_ISM.h"
+#import <UIKit/UIKit.h>
+#import "doUIModule.h"
+#import "doIUIModuleView.h"
+#import "doSingletonModule.h"
+
+@interface DoExt_Notification_SM : doSingletonModule<DoExt_Notification_ISM>
+
+@end
+
+@interface doConfirmView : UIAlertView <UIAlertViewDelegate>
+@property (nonatomic, strong)NSString *myCallBackName;
+@property (nonatomic, strong)id<doIScriptEngine> myScritEngine;
+-(instancetype)initWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles callbackName  :(NSString *)_callbackName scriptEngine:(id<doIScriptEngine>)_scritEngine;
+- (void)Dispose;
+@end
