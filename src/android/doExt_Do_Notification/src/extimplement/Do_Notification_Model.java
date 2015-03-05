0143024311a8e76593d319e06ee7319bda48f01f
zhuozuoying <zhuozuoying@163.com>
2015-03-05 10:39:05 +0800

    Update Do_Notification_Model.java
    修改注释

--- src/android/doExt_Do_Notification/src/extimplement/Do_Notification_Model.java
+++ src/android/doExt_Do_Notification/src/extimplement/Do_Notification_Model.java
@@ -10,11 +10,11 @@ import core.object.DoSingletonModule;
 import extdefine.Do_Notification_IMethod;
 
 /**
- * 自定义扩展API组件Model实现，继承DoSingletonModule抽象类，并实现Do_Notification_IMethod接口方法；
+ * 自定义扩展SM组件Model实现，继承DoSingletonModule抽象类，并实现Do_Notification_IMethod接口方法；
  * #如何调用组件自定义事件？可以通过如下方法触发事件：
  * this.model.getEventCenter().fireEvent(_messageName, jsonResult);
  * 参数解释：@_messageName字符串事件名称，@jsonResult传递事件参数对象；
- * 获取DoInvokeResult对象方式new DoInvokeResult();
+ * 获取DoInvokeResult对象方式new DoInvokeResult(this.model.getUniqueKey());
  */
 public class Do_Notification_Model extends DoSingletonModule implements Do_Notification_IMethod{
 
@@ -48,7 +48,7 @@ public class Do_Notification_Model extends DoSingletonModule implements Do_Notif
 	 * #如何执行异步方法回调？可以通过如下方法：
 	 * _scriptEngine.callback(_callbackFuncName, _invokeResult);
 	 * 参数解释：@_callbackFuncName回调函数名，@_invokeResult传递回调函数参数对象；
-	 * 获取DoInvokeResult对象方式new DoInvokeResult();
+	 * 获取DoInvokeResult对象方式new DoInvokeResult(this.model.getUniqueKey());
 	 */
 	@Override
 	public boolean invokeAsyncMethod(String _methodName, DoJsonNode _dictParas,
